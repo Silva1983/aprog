f751c82235192e2035ebaf2c1b4a88840b09069f
1171343 <1171343@isep.ipp.pt>
2018-11-06 15:46:39 +0000

    ex1 pla7

--- pl7/ex1.java
+++ pl7/ex1.java
@@ -19,37 +19,48 @@ public class ex1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       
+       /* 
+        O programa preenche um vector de 10 elementos introduzidos pelo utilizador, depois de preenchido,
+        verifica quais desses elementos são pares, calcula a quantidades, a sua soma e por último, a média
+        desses números.
+        */
         int i, s = 0, c = 0;
         int [] v = new int[10];
         
-        int menor = menorInteiro(v); 
+        
         Scanner ler = new Scanner (System.in);
         
         for (i = 0; i < v.length; i++){
             System.out.println("Número");
             v[i] = ler.nextInt();
         }
+        menorInteiro(v);
         
         for (i = 0; i < v.length; i++) {
             if (v[i] % 2 == 0) {
                 s = s + v[i];
                 c++;
+                System.out.println("A soma dos números pares é de: "+s);
             }
         }
         if (c != 0)
-            System.out.println(((double)s)/c);
+            System.out.println("A média dos números pares é de: " +((double)s)/c);
         else
             System.out.println("Operação impossível de realizar");
     }
     
-    public static int menorInteiro (int v[]){
-        int menor = 0;
+    public static void menorInteiro (int v[]){
+        int indice = 0;
+        int menor = v[0];
+        
         for (int i = 0; i <v.length; i++){
-            if (menor < v[i])
+            if (v[i] < menor){
                 menor = v[i];
-            
-        }
-        return menor;
+                indice = i+1;
+            }
+       }
+        System.out.println("O menor número do vetor é: "+ menor+ " e o indice desse valor é: " + indice);
     }
+    
+   
 }
